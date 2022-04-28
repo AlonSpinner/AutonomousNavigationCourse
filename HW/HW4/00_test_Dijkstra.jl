85a12a12ad70e5de6bac66a9a280dd015b636776
alonspinner <alonspinner@gmail.com>
2022-04-28 15:44:24 +0300

    update for HW4

---
+++ HW/HW4/00_test_Dijkstra.jl
@@ -0,0 +1,40 @@
+using Revise
+using Graphs #note: Graphs.jl is a reboot of the LightGraphs package which was archived
+using SimpleWeightedGraphs
+using GraphPlot
+includet("./graphForwardSearch.jl") #include and track changes
+
+#create graph
+#--------------------------------------------------------
+#note: Vertices locations dont matter when solving dijkstra
+V =  zeros(5,2)
+V[1,:] = [1,1]
+V[2,:] = [2,1]
+V[3,:] = [3,3]
+V[4,:] = [4,5]
+V[5,:] = [5,5]
+
+E = zeros(7,2)
+E[1,:] = [1,2]
+E[2,:] = [2,4]
+E[3,:] = [4,1]
+E[4,:] = [4,5]
+E[5,:] = [2,5]
+E[6,:] = [3,5]
+E[7,:] = [3,4]
+
+W = [1.5,2.5,2.6,2.3,2.4,3.0,4.0]
+
+graph = SimpleWeightedGraph(size(V,1))
+for i in 1:length(W)
+    add_edge!(graph, E[i,1], E[i,2], W[i])
+end
+#----------------------------------------------------
+s = 1
+τ = 5
+
+println("my solution:")
+println(Dijkstra(graph,s,τ))
+
+println("package solution:")
+println(enumerate_paths(dijkstra_shortest_paths(graph, s), τ))
---
+++ HW/HW4/graphForwardSearch.jl
@@ -0,0 +1,46 @@
+#https://juliagraphs.org/Graphs.jl/dev/
+using Graphs #note: Graphs.jl is a reboot of the LightGraphs package which was archived
+using SimpleWeightedGraphs
+
+mutable struct LocatedGraph
+    graph::SimpleWeightedGraph
+    locations::Matrix{Float64}
+end
+
+function Dijkstra(G, s, τ)
+    #openSet - the set of vertices that are at the frontier of the tree, and we think we should expand
+    #a vertex belongs to a set iff set[j]=true
+    openSet = zeros(Bool,ne(G))
+    openSet[s]  = true
+    #set all labels to inf except where we started
+    g = Inf*ones(Float64,ne(G)) 
+    g[s] = 0
+    #initalize parent array. parent[i] = j
+    parent = zeros(Int,ne(G)) 
+
+    while any(openSet)
+        #i = vertex in openSet with lowest g[i]
+        i = argmin(g .+ Inf.*.!(openSet))  #add inf to every label not in openSet and then argmin
+
+        openSet[i] = false  #remove i from openSet and add it to expandedSet
+        #for children j of vertex i:
+            #if there is a better way to reach j, update g[j], and add to openSet as it might be worth traveling through
+        for j in all_neighbors(G,i) 
+            c_ij = G.weights[i,j]
+            if g[i] + c_ij < g[j] && g[i]+c_ij < g[τ] #note: this will disqualify backtracking in the tree, so its ok we use all_neighbors
+                g[j] = g[i] + c_ij
+                parent[j] = i
+                if j != τ
+                    openSet[j] = true
+                end
+            end
+        end
+    end
+
+    #find shortestpath by backtracking from τ to s
+    shortestpath = [τ]
+    while shortestpath[end] != s
+        append!(shortestpath,parent[shortestpath[end]])
+    end
+    return shortestpath[end:-1:1] #flip
+end
