fab24c3cfffb9b701d26371e5e29819d1952e9c3
alonspinner <alonspinner@gmail.com>
2022-05-25 12:50:53 +0300

    started working on planner

---
+++ project/cbMDP/planner.py
@@ -0,0 +1,22 @@
+class planner():
+
+    def __init__(self):
+        self.alpha_LB
+        self.alpha_prev
+        self.Msigma
+        self.MxBar
+        self.MuBar
+        self.lambDa #stepsize for gradient decent
+        pass
+    
+    def outerLayer(): #plan
+        pass
+
+    def computeGradient():
+        pass
+
+    def evaluateObjective():
+        pass
+
+    def innerLayer():
+        pass
